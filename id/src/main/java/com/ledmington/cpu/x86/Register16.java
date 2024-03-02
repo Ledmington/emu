1056458e2982c9d505a1b50b51cee5e272b5b76d
Ledmington <filippo.barbari@gmail.com>
2024-03-02 19:25:05 +0100

    Added new 8-bit and 16-bit registers

--- id/src/main/java/com/ledmington/cpu/x86/Register16.java
+++ id/src/main/java/com/ledmington/cpu/x86/Register16.java
@@ -10,6 +10,14 @@ public final class Register16 extends Register {
     public static final Register16 DI = new Register16("di");
     public static final Register16 SP = new Register16("sp");
     public static final Register16 BP = new Register16("bp");
+    public static final Register16 R8W = new Register16("r8w");
+    public static final Register16 R9W = new Register16("r9w");
+    public static final Register16 R10W = new Register16("r10w");
+    public static final Register16 R11W = new Register16("r11w");
+    public static final Register16 R12W = new Register16("r12w");
+    public static final Register16 R13W = new Register16("r13w");
+    public static final Register16 R14W = new Register16("r14w");
+    public static final Register16 R15W = new Register16("r15w");
 
     private Register16(final String mnemonic) {
         super(mnemonic);
@@ -25,6 +33,14 @@ public final class Register16 extends Register {
             case 0x05 -> BP;
             case 0x06 -> SI;
             case 0x07 -> DI;
+            case 0x08 -> R8W;
+            case 0x09 -> R9W;
+            case 0x0a -> R10W;
+            case 0x0b -> R11W;
+            case 0x0c -> R12W;
+            case 0x0d -> R13W;
+            case 0x0e -> R14W;
+            case 0x0f -> R15W;
             default -> throw new IllegalArgumentException(String.format("Unknown register byte 0x%02x", b));
         };
     }
--- id/src/main/java/com/ledmington/cpu/x86/Register8.java
+++ id/src/main/java/com/ledmington/cpu/x86/Register8.java
@@ -10,6 +10,14 @@ public final class Register8 extends Register {
     public static final Register8 BH = new Register8("bh");
     public static final Register8 CH = new Register8("ch");
     public static final Register8 DH = new Register8("dh");
+    public static final Register8 R8B = new Register8("r8b");
+    public static final Register8 R9B = new Register8("r9b");
+    public static final Register8 R10B = new Register8("r10b");
+    public static final Register8 R11B = new Register8("r11b");
+    public static final Register8 R12B = new Register8("r12b");
+    public static final Register8 R13B = new Register8("r13b");
+    public static final Register8 R14B = new Register8("r14b");
+    public static final Register8 R15B = new Register8("r15b");
 
     public Register8(final String mnemonic) {
         super(mnemonic);
@@ -25,6 +33,14 @@ public final class Register8 extends Register {
             case 0x05 -> CH;
             case 0x06 -> DH;
             case 0x07 -> BH;
+            case 0x08 -> R8B;
+            case 0x09 -> R9B;
+            case 0x0a -> R10B;
+            case 0x0b -> R11B;
+            case 0x0c -> R12B;
+            case 0x0d -> R13B;
+            case 0x0e -> R14B;
+            case 0x0f -> R15B;
             default -> throw new IllegalArgumentException(String.format("Unknown register byte 0x%02x", b));
         };
     }
