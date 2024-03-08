1cc39a0d0290abc3c2eba9439da5a05398abfbf8
Ledmington <filippo.barbari@gmail.com>
2024-03-08 09:15:07 +0100

    Added opcode javadoc

--- id/src/main/java/com/ledmington/cpu/x86/Opcode.java
+++ id/src/main/java/com/ledmington/cpu/x86/Opcode.java
@@ -2,35 +2,154 @@ package com.ledmington.cpu.x86;
 
 import java.util.Objects;
 
+/**
+ * X86 opcode mnemonics.
+ */
 public enum Opcode {
+
+    /**
+     * Add.
+     */
     ADD("add"),
+
+    /**
+     * Logical AND.
+     */
     AND("and"),
+
+    /**
+     * Call procedure.
+     */
     CALL("call"),
+
+    /**
+     * Convert doubleword to quadword.
+     */
     CDQ("cdq"),
+
+    /**
+     * Conditional move if equal.
+     */
     CMOVE("cmove"),
+
+    /**
+     * Compare two operands.
+     */
     CMP("cmp"),
+
+    /**
+     * Signed multiply.
+     */
     IMUL("imul"),
+
+    /**
+     * Call to interrupt procedure.
+     */
     INT3("int3"),
+
+    /**
+     * Unsigned conditional jump if above.
+     */
     JA("ja"),
+
+    /**
+     * Unsigned conditional jump if equal.
+     */
     JE("je"),
+
+    /**
+     * Signed conditional jump if greater.
+     */
     JG("jg"),
+
+    /**
+     * Signed conditional jump if less or equal.
+     */
     JLE("jle"),
+
+    /**
+     * Unconditional jump.
+     */
     JMP("jmp"),
+
+    /**
+     * Unsigned conditional jump if not equal.
+     */
     JNE("jne"),
+
+    /**
+     * Conditional jump if signed.
+     */
     JS("js"),
+
+    /**
+     * Load effective address.
+     */
     LEA("lea"),
+
+    /**
+     * High-level procedure exit.
+     */
     LEAVE("leave"),
+
+    /**
+     * Move to/from registers and memory.
+     */
     MOV("mov"),
+
+    /**
+     * Move and zero-extend.
+     */
     MOVZX("movzx"),
+
+    /**
+     * No operation.
+     */
     NOP("nop"),
+
+    /**
+     * Logical OR.
+     */
     OR("or"),
+
+    /**
+     * Pop a value from the stack.
+     */
     POP("pop"),
+
+    /**
+     * Push word, doubleword or quadword onto the stack.
+     */
     PUSH("push"),
+
+    /**
+     * Return from procedure.
+     */
     RET("ret"),
+
+    /**
+     * Integer subtraction with borrow.
+     */
     SBB("sbb"),
+
+    /**
+     * Logical shift left.
+     */
     SHL("shl"),
+
+    /**
+     * Subtract.
+     */
     SUB("sub"),
+
+    /**
+     * Logical compare.
+     */
     TEST("test"),
+
+    /**
+     * Logical XOR.
+     */
     XOR("xor");
 
     private final String mnemonic;
