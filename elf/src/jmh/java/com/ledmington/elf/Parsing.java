c036f4271e33ba26851e17bc81a3605019842c88
Ledmington <filippo.barbari@gmail.com>
2024-06-21 11:00:18 +0200

    Added elf parsing benchmark

---
+++ elf/src/jmh/java/com/ledmington/elf/Parsing.java
@@ -0,0 +1,66 @@
+/*
+* emu - Processor Emulator
+* Copyright (C) 2023-2024 Filippo Barbari <filippo.barbari@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package com.ledmington.elf;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import com.ledmington.utils.MiniLogger;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Timeout;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@State(Scope.Benchmark)
+@BenchmarkMode({Mode.SampleTime})
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Timeout(time = 5)
+public class Parsing {
+
+    static {
+        MiniLogger.setMinimumLevel(MiniLogger.LoggingLevel.ERROR);
+    }
+
+    private static byte[] fileBytes;
+
+    @Setup
+    public void setup() {
+        try {
+            fileBytes = Objects.requireNonNull(Parsing.class.getClassLoader().getResourceAsStream("hello.elf"))
+                    .readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Benchmark
+    public void parse(final Blackhole bh) {
+        bh.consume(ELFReader.read(fileBytes));
+    }
+}
