bab5a1f6ad0bdcf109ca33088700ef61a6f1363e
Ledmington <filippo.barbari@gmail.com>
2024-08-31 00:11:27 +0200

    Added textarea for hex dump of file

--- emu-gui/src/main/java/com/ledmington/emu/ELFViewer.java
+++ emu-gui/src/main/java/com/ledmington/emu/ELFViewer.java
@@ -17,14 +17,75 @@
  */
 package com.ledmington.emu;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public final class ELFViewer extends Stage {
+
+    private final TextArea textArea = new TextArea();
+
     public ELFViewer(final double width, final double height) {
+        final VBox vbox = new VBox();
+        final Button load = new Button();
+        load.setText("Load");
+        load.setOnAction(e -> {
+            final FileChooser fc = new FileChooser();
+            fc.setTitle("Select an ELF file to be loaded");
+            fc.getExtensionFilters()
+                    .addAll(
+                            new FileChooser.ExtensionFilter("ELF files", "*.elf"),
+                            new FileChooser.ExtensionFilter("Shared libraries", "*.so"),
+                            new FileChooser.ExtensionFilter("Static libraries", "*.a"),
+                            new FileChooser.ExtensionFilter("All files", "*.*"));
+            final File selectedFile = fc.showOpenDialog(this);
+            if (selectedFile != null) {
+                this.loadFile(selectedFile);
+            }
+        });
+
+        textArea.setEditable(false);
+        textArea.setFont(new Font("Consolas", 12));
+        vbox.getChildren().addAll(load, textArea);
+        final Scene scene = new Scene(vbox);
+
+        this.setScene(scene);
         this.setTitle("ELF Viewer");
         this.setWidth(width);
         this.setHeight(height);
         this.centerOnScreen();
         this.show();
+    }
+
+    private void loadFile(final File elfFile) {
+        final byte[] fileBytes;
+        try {
+            fileBytes = Files.readAllBytes(elfFile.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < fileBytes.length; i++) {
+            if (i % 16 == 0) {
+                // start of the row
+                sb.append(" 0x").append(String.format("%08x", i)).append(" : ");
+            }
+            sb.append(String.format("%02x", fileBytes[i]));
+            if (i % 2 == 1) {
+                sb.append(' ');
+            }
+            if (i % 16 == 15) {
+                sb.append('\n');
+            }
+        }
+        this.textArea.setText(sb.toString());
     }
 }
