f465b6092a8bedebd6a75da201eca65b2f9cff77
Ledmington <ledmington.dev@gmail.com>
2024-11-02 22:29:37 +0100

    Minor fix

--- elf/src/main/java/com/ledmington/elf/section/note/NoteSectionEntryType.java
+++ elf/src/main/java/com/ledmington/elf/section/note/NoteSectionEntryType.java
@@ -57,24 +57,20 @@ public enum NoteSectionEntryType {
 	 */
 	public static NoteSectionEntryType fromCode(final String owner, final int type) {
 		return switch (owner) {
-			case "GNU" -> {
-				yield switch (type) {
-					case 1 -> NT_GNU_ABI_TAG;
-					case 2 -> NT_GNU_HWCAP;
-					case 3 -> NT_GNU_BUILD_ID;
-					case 4 -> NT_GNU_GOLD_VERSION;
-					case 5 -> NT_GNU_PROPERTY_TYPE_0;
-					default -> throw new IllegalArgumentException(String.format(
-							"Unknown note section entry type %d (0x%08x) for owner '%s'", type, type, owner));
-				};
-			}
-			case "stapsdt" -> {
-				yield switch (type) {
-					case 3 -> NT_STAPSDT;
-					default -> throw new IllegalArgumentException(String.format(
-							"Unknown note section entry type %d (0x%08x) for owner '%s'", type, type, owner));
-				};
-			}
+			case "GNU" -> switch (type) {
+				case 1 -> NT_GNU_ABI_TAG;
+				case 2 -> NT_GNU_HWCAP;
+				case 3 -> NT_GNU_BUILD_ID;
+				case 4 -> NT_GNU_GOLD_VERSION;
+				case 5 -> NT_GNU_PROPERTY_TYPE_0;
+				default -> throw new IllegalArgumentException(String.format(
+						"Unknown note section entry type %d (0x%08x) for owner '%s'", type, type, owner));
+			};
+			case "stapsdt" -> switch (type) {
+				case 3 -> NT_STAPSDT;
+				default -> throw new IllegalArgumentException(String.format(
+						"Unknown note section entry type %d (0x%08x) for owner '%s'", type, type, owner));
+			};
 			default -> throw new IllegalArgumentException(
 					String.format("Unknown note section entry owner '%s'", owner));
 		};
