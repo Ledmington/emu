8d194529b4357f070df3efb899fb6301bd328a15
Ledmington <ledmington.dev@gmail.com>
2025-02-20 21:45:08 +0100

    Added Encoding benchmark

---
+++ id/src/jmh/java/com/ledmington/id/Encoding.java
@@ -0,0 +1,69 @@
+/*
+ * emu - Processor Emulator
+ * Copyright (C) 2023-2025 Filippo Barbari <filippo.barbari@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.ledmington.id;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Timeout;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+import com.ledmington.cpu.x86.Immediate;
+import com.ledmington.cpu.x86.IndirectOperand;
+import com.ledmington.cpu.x86.Instruction;
+import com.ledmington.cpu.x86.InstructionEncoder;
+import com.ledmington.cpu.x86.Opcode;
+import com.ledmington.cpu.x86.PointerSize;
+import com.ledmington.cpu.x86.Register64;
+import com.ledmington.utils.MiniLogger;
+
+@State(Scope.Benchmark)
+@BenchmarkMode({Mode.SampleTime})
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Timeout(time = 5)
+public class Encoding {
+
+	static {
+		MiniLogger.setMinimumLevel(MiniLogger.LoggingLevel.ERROR);
+	}
+
+	private final Instruction inst = new Instruction(
+			Opcode.CMP,
+			IndirectOperand.builder()
+					.base(Register64.R9)
+					.index(Register64.RCX)
+					.constant(4)
+					.displacement(0x12345678)
+					.pointer(PointerSize.WORD_PTR)
+					.build(),
+			new Immediate((short) 0xbeef));
+
+	@Benchmark
+	public void encode(final Blackhole bh) {
+		bh.consume(InstructionEncoder.encode(inst));
+	}
+}
