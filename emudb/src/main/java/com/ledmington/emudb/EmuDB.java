190837f57026392afc5ed036045641b4f51644dc
Ledmington <ledmington.dev@gmail.com>
2025-10-04 13:09:27 +0200

    Improved levenshtein distance

--- emudb/src/main/java/com/ledmington/emudb/EmuDB.java
+++ emudb/src/main/java/com/ledmington/emudb/EmuDB.java
@@ -406,24 +406,27 @@ public final class EmuDB {
 			return m;
 		}
 
-		final int[][] dp = new int[m + 1][n + 1];
+		int[] prev = new int[n + 1];
+		int[] curr = new int[n + 1];
 
-		for (int i = 0; i <= m; i++) {
-			dp[i][0] = i;
-		}
-		for (int j = 0; j <= n; j++) {
-			dp[0][j] = j;
+		for (int i = 0; i <= n; i++) {
+			prev[i] = i;
 		}
 
-		// Fill the matrix
 		for (int i = 1; i <= m; i++) {
+			curr[0] = i;
+
 			for (int j = 1; j <= n; j++) {
 				final int cost = (a.charAt(i - 1) == b.charAt(j - 1)) ? 0 : 1;
-				dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + cost);
+				curr[j] = Math.min(Math.min(prev[j] + 1, curr[j - 1] + 1), prev[j - 1] + cost);
 			}
+
+			final int[] temp = prev;
+			prev = curr;
+			curr = temp;
 		}
 
-		return dp[m][n];
+		return prev[n];
 	}
 
 	private void runInteractively() {
