f64f492ab7654e5b5b8460032b6294484b36662b
Ledmington <ledmington.dev@gmail.com>
2025-10-20 17:45:33 +0200

    Minor fixes

--- id/src/main/java/com/ledmington/cpu/x86/InstructionEncoder.java
+++ id/src/main/java/com/ledmington/cpu/x86/InstructionEncoder.java
@@ -192,11 +192,11 @@ public final class InstructionEncoder {
 		final String opcode = inst.opcode().mnemonic();
 		sb.append(opcode);
 
-		if (opcode.length() < opcodePad) {
-			sb.append(" ".repeat(opcodePad - opcode.length()));
-		}
-		sb.append(' ');
 		if (inst.hasFirstOperand()) {
+			if (opcode.length() < opcodePad) {
+				sb.append(" ".repeat(opcodePad - opcode.length()));
+			}
+			sb.append(' ');
 			sb.append(operandString(inst, inst.firstOperand(), shortHex));
 			if (inst.hasDestinationMask()) {
 				sb.append('{').append(inst.getDestinationMask().toIntelSyntax()).append('}');
