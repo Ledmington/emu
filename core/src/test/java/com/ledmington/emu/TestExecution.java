e062ea6b6da3058ffa96942bdfb535edbd5ccaf6
Ledmington <ledmington.dev@gmail.com>
2025-11-13 00:36:07 +0100

    Fixed TestExecution

--- core/src/test/java/com/ledmington/emu/TestExecution.java
+++ core/src/test/java/com/ledmington/emu/TestExecution.java
@@ -210,6 +210,7 @@ final class TestExecution {
 		expected.set(RFlags.ZERO, result == 0L);
 		expected.set(RFlags.PARITY, (Long.bitCount(result) % 2) == 0);
 		expected.set(RFlags.SIGN, result < 0L);
+		expected.set(RFlags.CARRY, MathUtils.willCarry(oldValue1, oldValue2));
 		cpu.executeOne(new Instruction(Opcode.ADD, a, b));
 		assertEquals(
 				expected,
@@ -228,6 +229,7 @@ final class TestExecution {
 		expected.set(RFlags.ZERO, result == 0);
 		expected.set(RFlags.PARITY, (Integer.bitCount(result) % 2) == 0);
 		expected.set(RFlags.SIGN, result < 0);
+		expected.set(RFlags.CARRY, MathUtils.willCarry(oldValue1, oldValue2));
 		cpu.executeOne(new Instruction(Opcode.ADD, a, b));
 		assertEquals(
 				expected,
@@ -246,6 +248,7 @@ final class TestExecution {
 		expected.set(RFlags.ZERO, result == 0);
 		expected.set(RFlags.PARITY, (Integer.bitCount(BitUtils.asInt(result)) % 2) == 0);
 		expected.set(RFlags.SIGN, result < 0);
+		expected.set(RFlags.CARRY, MathUtils.willCarry(oldValue1, oldValue2));
 		cpu.executeOne(new Instruction(Opcode.ADD, a, b));
 		assertEquals(
 				expected,
@@ -264,6 +267,7 @@ final class TestExecution {
 		expected.set(RFlags.ZERO, result == 0);
 		expected.set(RFlags.PARITY, (Integer.bitCount(BitUtils.asInt(result)) % 2) == 0);
 		expected.set(RFlags.SIGN, result < 0);
+		expected.set(RFlags.CARRY, MathUtils.willCarry(oldValue1, oldValue2));
 		cpu.executeOne(new Instruction(Opcode.ADD, a, b));
 		assertEquals(
 				expected,
